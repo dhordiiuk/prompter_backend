77e8379543c4e20089e8648c08671349f237cbe5
dhordiiuk <dgordiuk@gmail.com>
2020-11-17 00:25:44 +0200

    add frontend builded files

---
+++ frontend/build/precache-manifest.2f86dae41b03b8526834ad6b7b8e2e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64345cb3f4700872a37ed6f2e8bc62e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d4a1750e2904cb1c5d",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f03b15fecdf26ac6a5d9",
+    "url": "/static/css/main.a345a27c.chunk.css"
+  },
+  {
+    "revision": "c5d4a1750e2904cb1c5d",
+    "url": "/static/js/2.07b99721.chunk.js"
+  },
+  {
+    "revision": "6053e451d7862a9dc5b0526c27e6d0db",
+    "url": "/static/js/2.07b99721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03b15fecdf26ac6a5d9",
+    "url": "/static/js/main.8b751330.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
